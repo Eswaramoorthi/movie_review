a27179e97dd55726106f1b5f6cddc06f71d0a7d4
eswaramoorthi94cse@gmail.com <pkgobi@gmail.com>
2019-06-12 14:06:08 +0530

    checking condition

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,13 @@
 class Review < ApplicationRecord
+	validate :movie_review_validate
+
+	validates :user_id, uniqueness: { scope: :movie_id}
 	belongs_to :user
 	belongs_to :movie
+
+	def movie_review_validate
+	  	if movie.user == self.user
+	  		errors.add(:movie, "not able to review the movie")
+	  	end
+	end
 end
